b307ba68902dc31a78ff046558a6dbf0eb9aa5a9
Tyrell <tyrellkamara-oliver@pursuit.org>
2023-07-12 09:05:57 -0400

    Trying to get the in stock button to work

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# Welcome to *Some Shop Name!*
+# *Nature's Medicine!*
+Welcome to the Herbal Inventory Website! This web application allows users to manage an inventory of herbs. Users can input new herbs, view the existing inventory, delete herbs, and toggle the availability of each herb (in stock or out of stock).
 
-![Picture of a green heart in the shape of a kettle.](https://file%252B.vscode-resource.vscode-cdn.net/Users/tyrellkamara-oliver/Pursuit/Module_2/Projects/Inventory-Web-Application-Project/Images/download.jpg?version%253D1687919411411)
+![Picture of a plant in the palm of a hand with the arm shaped like the letter "C".](/Images/HerbPic1.jpeg)
+
+## Features
+The Herbal Inventory Website provides the following features:
+
+1. **Add New Herbs:** Users can add new herbs to the inventory by providing the herb's name, description, price, origin, and image url.
+
+1. **View Inventory:** The website displays the current inventory of herbs, including their names, descriptions, price, etc.
+
+1. **Delete Herbs:** Users can remove herbs from the inventory. This action permanently deletes the herb from the database.
+
+1. **Toggle Availability:** Users can toggle the availability of each herb, marking it as either in stock or out of stock.
--- main.js
+++ main.js
@@ -2,16 +2,7 @@ const allProducts = document.querySelector(".all-products"); // Selecting the co
 
 const submitButton = document.querySelector("#submit"); // Selecting the submit input
 submitButton.addEventListener("click", () => {
-
     addHerb();
-
-
-
-    // const createNewElement = document.createElement("span"); // Creates the new element
-    // createNewElement.innerText = "Something"; // Gives the element something, in this case, text
-    // allProducts.appendChild(createNewElement); // Puts that new element onto the ".all-products" div container 
-
-    // allProducts.appendChild(herbName)
 });
 
 // const herbName = document.getElementById("name").value
@@ -19,39 +10,50 @@ const addHerb = () => {
     const name = document.querySelector("#name").value
     const price = document.querySelector("#price").value
     const origin = document.querySelector("#origin").value
+    const description = document.querySelector("#description").value
     const image = document.querySelector("#img").value
 
-    // const deleteButton = document.querySelector("#delete")
-    // deleteButton.addEventListener("click", () => {
-    //     deleteButton.removeChild(allProducts)
-    // });
-
     const createNewElement = document.createElement("div");
-    createNewElement.classList.add("Product3")
+    createNewElement.classList.add("herb-product");
     createNewElement.innerHTML = `
     <img id="pic3" src="${image}" />
+    <div class="product-info3"
         <h2>${name}</h2>
-        <p>${price}</p>
-        <p> ${origin}</p>
+        <p>$${price}</p>
+        <p>Origin: ${origin}</p>
+        <p id="product3-p">${description}</p>
+        <input id="delete" type="button" value="Delete" />
+        <input id="in-Stock" type="button" value="InStock" />
+
     `;
 
-    const childDiv = document.createElement("div")
-    childDiv.classList.add("product-info3")
+    const deleteButton = createNewElement.querySelector("#delete");
+    deleteButton.addEventListener("click", () => {
+        createNewElement.remove();
+    });
 
     allProducts.appendChild(createNewElement);
 };
 
-const newEle = document.createElement("h2")
-newEle
-
 
 //// The Reset Button
-const resetForm = document.querySelector(".new-herb")
+const resetForm = document.querySelector(".new-herb");
 
-const resetButton = document.querySelector("#reset")
+const resetButton = document.querySelector("#reset");
 
 resetButton.addEventListener("click", () => {
     resetForm.addEventListener("reset")
-})
+});
+
+//// The Delete Button
+const herbProduct = document.querySelector(".herb-product")
+
+const deleteButton = document.querySelector("#delete");
+    deleteButton.addEventListener("click", () => {
+        herbProduct.remove();
+    });
 
 
+const inStock = document.querySelector("#in-Stock");
+console.log(inStock);
+inStock.addEventListener("click", () => {})
